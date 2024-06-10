fd7fbfb67596d0cd72438b9df67d9eefde7095cb
Aleksandr Makarov <gif.inoe@gmail.com>
2024-06-10 23:36:09 +0300

    Use sqllite in db connection

--- AzureMarketplaceIntegrationSample/Program.cs
+++ AzureMarketplaceIntegrationSample/Program.cs
@@ -1,6 +1,10 @@
+using AzureMarketplaceIntegrationSample;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Marketplace.SaaS;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -8,6 +12,12 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddScoped<IMarketplaceSaaSClient, MarketplaceSaaSClient>(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            return new MarketplaceSaaSClient(new Azure.Identity.ClientSecretCredential(config["TenantId"], config["ClientId"], config["ClientSecret"]));
+        });
+        services.AddDbContext<CompanyDbContext>(options => options.UseSqlite("Data Source=companydb.sqlite"));
     })
     .Build();
 
